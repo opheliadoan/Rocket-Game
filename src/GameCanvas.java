b1d4a66523883ab6c4bdaad175eedb5492ffdaf0
opehliadoan <zikzakdoan@gmail.com>
2018-06-18 07:39:22 +0700

    Update

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -9,10 +9,29 @@ import java.io.IOException;
 public class GameCanvas extends JPanel {
 
     BufferedImage starImage;
+    BufferedImage enemyImage;
+    BufferedImage playerImage;
+
+    //BackBuffered
+    BufferedImage backBuffered;
+    Graphics graphics;
+
+    public int positionXStar = 400;
+    public int positionYStar = 150;
+
+    public int positionXEnemy = 600;
+    public int positionYEnemy = 200;
+
+    public int positionXPlayer = 500;
+    public int positionYPlayer = 300;
 
     public GameCanvas() {
         //Set up the Canvas
         this.setSize(1024, 600);
+
+        this.backBuffered = new BufferedImage(1024, 600, BufferedImage.TYPE_4BYTE_ABGR);
+        //get the graphics from the backBuffered
+        this.graphics = this.backBuffered.getGraphics();
 
         //Load the star image
         try {
@@ -21,21 +40,44 @@ public class GameCanvas extends JPanel {
             e.printStackTrace();
         }
 
+        try {
+            this.enemyImage = ImageIO.read(new File("resources/images/circle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.playerImage = ImageIO.read(new File("resources/images/circle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         this.setVisible(true);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
+        //Draw the images on the backBuffered
+        g.drawImage(this.backBuffered, 0, 0,null);
+
+    }
+
+    public void renderAll() {
         //super.paintComponent(g);
-        //Color color = new Color() : according to RB
+        //Color color = new Color(): according to RB
 
         //Draw the background
-        g.setColor(Color.BLACK);
+        this.graphics.setColor(Color.BLACK);
         //draw a rectangular
-        g.fillRect(0,0,1024,600);
+        this.graphics.fillRect(0,0,1024,600);
 
         //Add the details
-        g.drawImage(this.starImage, 400, 200, null);
+        this.graphics.drawImage(this.starImage, this.positionXStar, this.positionYStar, 5, 5,null);
+        this.graphics.drawImage(this.enemyImage, this.positionXEnemy, this.positionYEnemy, 20, 20, null);
+        this.graphics.drawImage(this.playerImage, this.positionXPlayer, this.positionYPlayer, 20, 20, null);
+
+        this.repaint();
     }
 
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,17 +1,79 @@
+/**
+ * Name: Ophelia Doan
+ * Date: 170618
+ */
+
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GameWindow extends JFrame {
 
     GameCanvas gameCanvas;
+    long lastTime = 0;
 
     public GameWindow() {
         //Set up sizes
         this.setSize(1024, 600);
 
-        //pass the Canvas into the game window
+        //pass the Canvas into the gameWindow
         this.gameCanvas = new GameCanvas();
         this.add(this.gameCanvas);
 
+        //Read the movement from the keyboard to move the player
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    gameCanvas.positionXPlayer -= 8;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    gameCanvas.positionXPlayer += 8;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                //System.out.println("keyReleased");
+            }
+        });
+
+        //Exit when push red x
+       this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(1);
+            }
+        });
+
         this.setVisible(true);
+    }
+
+    //Make the imgaes of thebackground move
+    //Delay the speed of the star so it is visible
+    public void gameLoop() {
+        while(true) {
+            //unix time
+            long currTime = System.nanoTime();
+            if (currTime - this.lastTime >= 17_000_000) {
+                //Move the star
+                this.gameCanvas.positionXStar -= 3;
+
+                //Move the enemy
+                this.gameCanvas.positionYEnemy -= 4;
+
+                //Move the player
+                //this.gameCanvas.positionXPlayer -= 4;
+
+                this.gameCanvas.renderAll();
+                this.lastTime = currTime;
+            }
+        }
     }
 }
